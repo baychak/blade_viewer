4110cfa95b8c89cde2b307c858e9944b21775136
Mykola Baichak <mykola.baichak@globallogic.com>
2019-06-03 22:50:02 +0300

    Incline blade

--- prepare_img_info.R
+++ prepare_img_info.R
@@ -71,21 +71,27 @@ sides <- c("leading-edge", "suction-side", "trailing-edge", "pressure-side")
 directions <- list(c(-1,0,0), c(0,-1,0), c(1,0,0), c(0,1,0))
 names(directions) <- sides
 
+object.angles <- c(0, -0.28, 2, 2.02) # in degrees
+names(object.angles) <- sides
+object.angles <- object.angles*pi/180
+
 
 getYawAngle <- function(zCamVector, mainDirection) {
     zCamVector[3] <- 0
     getAngle(zCamVector, mainDirection, c(0,0,-1))
 }
 
-object.center = c(1.0, 0.5)
+object.center = c(1.0, 0.5, 0)
+# object.angle = (90 - 87.28)*pi/180
+initZ = 30
 
-getDistance <- function(xyPoint) {
-    sqrt(sum((xyPoint-object.center)^2))
+getDistance <- function(xyzPoint, direction, object.angle) {
+    (xyzPoint - object.center) %*% -direction + (xyzPoint[3] - initZ)*tan(object.angle)
 }
 
 # imageSize <- c(5456, 3632)
 
-prepareSideData <- function(side, direction) {
+prepareSideData <- function(side, direction, object.angle) {
     data <- metadata[blade_side == side]
     data <- data[order(file)]
     coords <- data[, .(file, cam_to_blade_rbt)]
@@ -97,16 +103,18 @@ prepareSideData <- function(side, direction) {
                 sapply(cam_to_blade_rbt, function(mat) {getPitchRollAngles(mat)[1]}),
                 sapply(cam_to_blade_rbt, function(mat) {getPitchRollAngles(mat)[2]}),
                 sapply(cam_to_blade_rbt, function(mat) {getYawAngle(mat[1:3, 3], direction)}),
-                sapply(cam_to_blade_rbt, function(mat) {getDistance(mat[1:2, 4])})
+                sapply(cam_to_blade_rbt, function(mat) {getDistance(mat[1:3, 4], direction, object.angle)})
             )
           ]
-    coords$z <- coords$z - tan(coords$pitch)*coords$distance
+
+    coords$z <- coords$z - coords$distance/(tan(object.angle) + 1/tan(coords$pitch))
+    coords$distance <- coords$distance/(tan(object.angle) * tan(coords$pitch) + 1)
     plot(coords$x, coords$y, type = "b", asp = 1)
     coords
 }
 
 ###### leading-edge
-coords.leading.edge <- prepareSideData("leading-edge", directions$`leading-edge`)
+coords.leading.edge <- prepareSideData("leading-edge", directions$`leading-edge`, object.angles["leading-edge"])
 distanceToBlade <- 7.0 # 20 and 21 snapshot of leading edge
 snapshot20_21_z_delta <- coords.leading.edge$z[21] - coords.leading.edge$z[20] # in meters
 snapshot20_21_Ydelta <- 3565 - 407 # in pixels
@@ -123,16 +131,17 @@ ProjectionZ <- matrix(c(1,0,0,0,
                       nrow = 4, ncol = 4, byrow = T)
 
 getTransformation <- function(roll, pitch, R, xShift, yaw, distance) {
-    ReconstructionY <- matrix(c(1,0,0,0,
-                                0,1,tan(pitch),-tan(pitch)/R,
-                                0,0,0,0,
-                                0,0,0,1),
-                              nrow = 4, ncol = 4, byrow = T)
+    # pitch <- pitch - objAngle
     RotateZ <- matrix(c(cos(-roll),sin(-roll),0,0,
                         -sin(-roll),cos(-roll),0,0,
                         0,0,1,0,
                         0,0,0,1),
                       nrow = 4, ncol = 4, byrow = T)
+    ReconstructionY <- matrix(c(1,0,0,0,
+                                0,1,tan(pitch),-tan(pitch)/R,
+                                0,0,0,0,
+                                0,0,0,1),
+                              nrow = 4, ncol = 4, byrow = T)
     RotateX <- matrix(c(1,0,0,0,
                         0,cos(-pitch),sin(-pitch),0,
                         0,-sin(-pitch),cos(-pitch),0,
@@ -173,10 +182,10 @@ getTransformation <- function(roll, pitch, R, xShift, yaw, distance) {
     Transformation
 }
 
-saveAndShowStitchData <- function(data, side) {
+saveAndShowStitchData <- function(data, side, object.angle) {
     data[,
         transformation := mapply(
-            function(rAngle, pAngle, xSh, yAngle, dist) {getTransformation(rAngle, pAngle, R, xSh, yAngle, dist)},
+            function(rAngle, pAngle, xSh, yAngle, dist) {getTransformation(rAngle, pAngle-object.angle, R, xSh, yAngle, dist)},
             roll,
             pitch,
             xShift,
@@ -191,22 +200,22 @@ saveAndShowStitchData <- function(data, side) {
     debugData
 }
 coords.leading.edge$xShift <- coords.leading.edge$y
-le <- saveAndShowStitchData(coords.leading.edge, "leading-edge")
+le <- saveAndShowStitchData(coords.leading.edge, "leading-edge", object.angles["leading-edge"])
 
 ###### suction-side
-coords.suction.side <- prepareSideData("suction-side", directions$`suction-side`)
+coords.suction.side <- prepareSideData("suction-side", directions$`suction-side`, object.angles["suction-side"])
 coords.suction.side$xShift <- -coords.suction.side$x
-ss <- saveAndShowStitchData(coords.suction.side, "suction-side")
+ss <- saveAndShowStitchData(coords.suction.side, "suction-side", object.angles["suction-side"])
 
 ###### trailing-edge
-coords.trailing.edge <- prepareSideData("trailing-edge", directions$`trailing-edge`)
+coords.trailing.edge <- prepareSideData("trailing-edge", directions$`trailing-edge`, object.angles["trailing-edge"])
 coords.trailing.edge$xShift <- -coords.trailing.edge$y
-te <- saveAndShowStitchData(coords.trailing.edge, "trailing-edge")
+te <- saveAndShowStitchData(coords.trailing.edge, "trailing-edge", object.angles["trailing-edge"])
 
 ###### pressure-side
-coords.pressure.side <- prepareSideData("pressure-side", directions$`pressure-side`)
+coords.pressure.side <- prepareSideData("pressure-side", directions$`pressure-side`, object.angles["pressure-side"])
 coords.pressure.side$xShift <- coords.pressure.side$x
-ps <- saveAndShowStitchData(coords.pressure.side, "pressure-side")
+ps <- saveAndShowStitchData(coords.pressure.side, "pressure-side", object.angles["pressure-side"])
 
 ###### make and run blade_viewer
 # system2("bash", args = c("run_blade_viewer.sh", toString(scaleFactor)))
